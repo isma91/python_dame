e62346e49aad58f193cdd2ee2c4459e7690e07ef
isma91 <ismaydogmus@gmail.com>
2016-05-23 09:35:01 +0200

    display windows by the user width and height

---
+++ index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry('{}x{}'.format(int(root.winfo_screenwidth() / 2), int(root.winfo_screenheight() / 2)))
+root.wm_title("Python Checkers")
+
+
+root.mainloop()
